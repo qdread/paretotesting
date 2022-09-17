ac77110bad2beb1d4f2b1fd18370f37c110ade42
Quentin Read <quentin.read@usda.gov>
2022-09-17 15:14:50 -0400

    more try on diameter J curve

--- diam_jcurve_fixef_brm_remote.R
+++ diam_jcurve_fixef_brm_remote.R
@@ -2,10 +2,8 @@
 
 library(dplyr)
 library(readr)
-#library(tidybayes)
 library(brms)
-#library(forestscaling)
-#library(purrr)
+
 
 options(mc.cores = 4, brms.backend = 'cmdstanr', brms.file_refit = 'on_change')
 
@@ -16,24 +14,6 @@ diam_data <- alltreedat[[3]] %>%
   filter(!is.na(fg)) %>%
   select(fg, dbh_corr, diam_growth_rate) %>%
   mutate(fg = paste0('fg', fg))
-
-# ### Visualize trends before writing model
-# # Set number of bins       
-# numbins <- 20
-# 
-# # Make a version of alltreedat without the unclassified trees
-# alltreedat_classified <- map(alltreedat, ~ filter(., !is.na(fg)))
-# 
-# # Bin classified trees. (log binning of density)
-# allyeardbh_classified <- map(alltreedat_classified[-1], ~ pull(., dbh_corr)) %>% unlist
-# dbhbin_allclassified <- logbin(x = allyeardbh_classified, y = NULL, n = numbins)
-# 
-# diam_bins <- diam_data %>% 
-#   group_by(fg) %>%
-#   group_modify(~ cloudbin_across_years(dat_classes = .$dbh_corr, dat_values = .$diam_growth_rate, edges = dbhbin_allclassified, n_census = 1))
-# 
-# ggplot(diam_bins, aes(x = bin_midpoint, y = median, ymin = q25, ymax = q75)) +
-#   geom_pointrange() + facet_wrap(~fg) + scale_x_log10() + scale_y_log10()
 
 diam_jcurve_fixef_fit <- brm(
   bf(
@@ -52,4 +32,57 @@ diam_jcurve_fixef_fit <- brm(
   chains = 4, iter = 3000, warmup = 2000, seed = 27704,
   file = '~/temp/forestlight/diam_jcurve_fixef_brmfit'
 )
+
+### The above model doesn't well capture the actual J shape at low sizes. It may be necessary to change the functional form.
+
+
+# Postprocessing and plotting locally -------------------------------------
+
+library(tidybayes)
+library(ggplot2)
+library(forestscaling)
+library(purrr)
+
+# Set number of bins
+numbins <- 20
+
+# Make a version of alltreedat without the unclassified trees
+alltreedat_classified <- map(alltreedat, ~ filter(., !is.na(fg)))
+
+# Bin classified trees. (log binning of density)
+allyeardbh_classified <- map(alltreedat_classified[-1], ~ pull(., dbh_corr)) %>% unlist
+dbhbin_allclassified <- logbin(x = allyeardbh_classified, y = NULL, n = numbins)
+
+diam_bins <- diam_data %>%
+  group_by(fg) %>%
+  group_modify(~ cloudbin_across_years(dat_classes = .$dbh_corr, dat_values = .$diam_growth_rate, edges = dbhbin_allclassified, n_census = 1))
+
+# Prediction grid: dbh x fg
+# Limit ranges to the observed data points
+diam_max <- diam_bins %>%
+  filter(!is.na(mean)) %>%
+  group_by(fg) %>%
+  filter(bin_midpoint == max(bin_midpoint))
+
+pred_dat <- diam_max %>%
+  group_by(fg) %>%
+  group_modify(~ data.frame(dbh_corr = exp(seq(log(1), log(.$bin_midpoint), length.out = 50))))
+
+# Get epred values
+jcurve_pred <- pred_dat %>%
+  add_epred_draws(diam_jcurve_fixef_fit) %>%
+  mutate(.epred = exp(.epred))
+
+ggplot(diam_bins, aes(x = bin_midpoint, y = median, ymin = q25, ymax = q75)) +
+  geom_pointrange() + facet_wrap(~fg) + scale_x_log10() + scale_y_log10()
+
+
+### Quick diag. plot
+ggplot(diam_bins, aes(x=bin_midpoint, y = median)) +
+  stat_lineribbon(aes(y = .epred, x = dbh_corr), data = jcurve_pred) +
+  geom_pointrange(aes(ymin = q25, ymax = q75), color = 'gray40') +
+  facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10() + 
+  scale_fill_brewer(palette = 'Blues') + 
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
 
