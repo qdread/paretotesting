20b8d1f8bf9580d55410b5726223d5c216170e88
Quentin Read <quentin.read@usda.gov>
2022-09-20 20:14:14 -0400

    hinge thing works

--- diam_loglin_fixef_brm.R
+++ diam_loglin_fixef_brm.R
@@ -56,63 +56,87 @@ diam_hinge_fixef_fit <- brm(
 
 # Postprocessing and plotting locally -------------------------------------
 
-# library(tidybayes)
-# library(ggplot2)
-# library(forestscaling)
-# library(purrr)
-# library(tidyr)
-# 
-# # Set number of bins
-# numbins <- 20
-# 
-# # Make a version of alltreedat without the unclassified trees
-# alltreedat_classified <- map(alltreedat, ~ filter(., !is.na(fg)))
-# 
-# # Bin classified trees. (log binning of density)
-# allyeardbh_classified <- map(alltreedat_classified[-1], ~ pull(., dbh_corr)) %>% unlist
-# dbhbin_allclassified <- logbin(x = allyeardbh_classified, y = NULL, n = numbins)
-# bin_edges <- c(dbhbin_allclassified$bin_min,dbhbin_allclassified$bin_max[numbins])
-# 
-# qprobs <- c(0.025, 0.25, 0.5, 0.75, 0.975)
-# diam_bins <- diam_data %>% 
-#   mutate(dbh_bin = cut(dbh_corr, breaks = bin_edges, include.lowest = TRUE)) %>%
-#   group_by(fg, dbh_bin) %>%
-#   summarize(p = qprobs, q = quantile(diam_growth_rate, probs = qprobs), n = n()) %>%
-#   pivot_wider(names_from = p, values_from = q) %>%
-#   setNames(c('fg', 'dbh_bin', 'n', 'q025', 'q25', 'median', 'q75', 'q975')) %>%
-#   mutate(bin_midpoint = dbhbin_allclassified$bin_midpoint[as.numeric(dbh_bin)])
-# 
-# 
-# # Prediction grid: dbh x fg
-# # Limit ranges to the observed data points and bins with > 20
-# diam_max <- diam_bins %>%
-#   filter(n >= 20) %>%
-#   group_by(fg) %>%
-#   filter(bin_midpoint == max(bin_midpoint))
-# 
-# pred_dat <- diam_max %>%
-#   group_by(fg) %>%
-#   group_modify(~ data.frame(dbh_corr = exp(seq(log(1), log(.$bin_midpoint), length.out = 101))))
-# 
-# # Get epred values
-# diamloglin_pred <- pred_dat %>%
-#   add_epred_draws(diam_loglin_fixef_fit) %>%
-#   mutate(.epred = exp(.epred))
-# 
-# ### Quick diag. plot
-# ggplot(diam_bins %>% filter(n >= 20), aes(x=bin_midpoint, y = median)) +
-#   stat_lineribbon(aes(y = .epred, x = dbh_corr), data = diamloglin_pred) +
-#   geom_pointrange(aes(ymin = q25, ymax = q75), color = 'gray40') +
-#   facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10() +
-#   scale_fill_brewer(palette = 'Blues') +
-#   theme_bw() +
-#   theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
-# 
-# ### Plot with all data points
-# ggplot(diam_data, aes(x = dbh_corr, y = diam_growth_rate)) +
-#   geom_point(size = 0.5, alpha = 0.1) +
-#   stat_lineribbon(aes(y = .epred, x = dbh_corr), data = diamloglin_pred, color = 'blue4') +
-#   facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10() +
-#   scale_fill_brewer(palette = 'Blues') +
-#   theme_bw() +
-#   theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
+library(tidybayes)
+library(ggplot2)
+library(forestscaling)
+library(purrr)
+library(tidyr)
+
+# Set number of bins
+numbins <- 20
+
+# Make a version of alltreedat without the unclassified trees
+alltreedat_classified <- map(alltreedat, ~ filter(., !is.na(fg)))
+
+# Bin classified trees. (log binning of density)
+allyeardbh_classified <- map(alltreedat_classified[-1], ~ pull(., dbh_corr)) %>% unlist
+dbhbin_allclassified <- logbin(x = allyeardbh_classified, y = NULL, n = numbins)
+bin_edges <- c(dbhbin_allclassified$bin_min,dbhbin_allclassified$bin_max[numbins])
+
+qprobs <- c(0.025, 0.25, 0.5, 0.75, 0.975)
+diam_bins <- diam_data %>%
+  mutate(dbh_bin = cut(dbh_corr, breaks = bin_edges, include.lowest = TRUE)) %>%
+  group_by(fg, dbh_bin) %>%
+  summarize(p = qprobs, q = quantile(diam_growth_rate, probs = qprobs), n = n()) %>%
+  pivot_wider(names_from = p, values_from = q) %>%
+  setNames(c('fg', 'dbh_bin', 'n', 'q025', 'q25', 'median', 'q75', 'q975')) %>%
+  mutate(bin_midpoint = dbhbin_allclassified$bin_midpoint[as.numeric(dbh_bin)])
+
+
+# Prediction grid: dbh x fg
+# Limit ranges to the observed data points and bins with > 20
+diam_max <- diam_bins %>%
+  filter(n >= 20) %>%
+  group_by(fg) %>%
+  filter(bin_midpoint == max(bin_midpoint))
+
+pred_dat <- diam_max %>%
+  group_by(fg) %>%
+  group_modify(~ data.frame(dbh_corr = exp(seq(log(1), log(.$bin_midpoint), length.out = 101))))
+
+# Get epred values
+diamloglin_pred <- pred_dat %>%
+  add_epred_draws(diam_loglin_fixef_fit) %>%
+  mutate(.epred = exp(.epred))
+
+diamhinge_pred <- pred_dat %>%
+  add_epred_draws(diam_hinge_fixef_fit) %>%
+  mutate(.epred = exp(.epred))
+
+### LOGLIN
+### Quick diag. plot
+ggplot(diam_bins %>% filter(n >= 20), aes(x=bin_midpoint, y = median)) +
+  stat_lineribbon(aes(y = .epred, x = dbh_corr), data = diamloglin_pred, size = 0.5) +
+  geom_pointrange(aes(ymin = q25, ymax = q75), color = 'gray40') +
+  facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10(name = 'diameter growth (cm/y)') +
+  scale_fill_brewer(palette = 'Blues') +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
+
+### Plot with all data points
+ggplot(diam_data, aes(x = dbh_corr, y = diam_growth_rate)) +
+  geom_point(size = 0.5, alpha = 0.1) +
+  stat_lineribbon(aes(y = .epred, x = dbh_corr), data = diamloglin_pred, color = 'blue4', size = 0.5) +
+  facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10(name = 'diameter growth (cm/y)') +
+  scale_fill_brewer(palette = 'Blues') +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
+
+### HINGE
+### Quick diag. plot
+ggplot(diam_bins %>% filter(n >= 20), aes(x=bin_midpoint, y = median)) +
+  stat_lineribbon(aes(y = .epred, x = dbh_corr), data = diamhinge_pred, size = 0.5) +
+  geom_pointrange(aes(ymin = q25, ymax = q75), color = 'gray40') +
+  facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10(name = 'diameter growth (cm/y)') +
+  scale_fill_brewer(palette = 'Blues') +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
+
+### Plot with all data points
+ggplot(diam_data, aes(x = dbh_corr, y = diam_growth_rate)) +
+  geom_point(size = 0.5, alpha = 0.1) +
+  stat_lineribbon(aes(y = .epred, x = dbh_corr), data = diamhinge_pred, color = 'blue4', size = 0.5) +
+  facet_wrap(~ fg) + scale_x_log10(name = 'diameter (cm)') + scale_y_log10(name = 'diameter growth (cm/y)') +
+  scale_fill_brewer(palette = 'Blues') +
+  theme_bw() +
+  theme(legend.position = c(0.8, 0.2), strip.background = element_blank())
